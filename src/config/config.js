b1d96e4e4409765a8fe6843b88320628e48cebc8
Lorenzo Bergami <lorebergam@gmail.com>
2026-01-31 10:53:42 +0100

    fixed ports

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 3001,
-    notificationServiceUrl: process.env.NOTIFICATION_SERVICE_URL || 'http://localhost:3012',
     rulesServiceUrl: process.env.RULES_SERVICE_URL || 'http://localhost:3012'
 };
